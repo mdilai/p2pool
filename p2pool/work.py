6881c5b7118466563ac818e5b8e3c4cf95afa7a3
Jonathan Roy <jonroy7@gmail.com>
2014-02-12 18:26:59 +0000

    local_addr_rates may not have been in scope, fetch them instead

--- p2pool/work.py
+++ p2pool/work.py
@@ -389,7 +389,7 @@ class WorkerBridge(worker_interface.WorkerBridge):
                     pubkey_hash,
                     bitcoin_data.target_to_difficulty(target),
                     bitcoin_data.target_to_difficulty(share_info['bits'].target),
-                    local_addr_rates.get(pubkey_hash, 0),
+                    self.get_local_addr_rates().get(pubkey_hash, 0),
                     self.current_work.value['subsidy']*1e-8, self.node.net.PARENT.SYMBOL,
                     len(self.current_work.value['transactions']),
                     sum(map(bitcoin_data.tx_type.packed_size, self.current_work.value['transactions']))/1000.,
