3f4613a0e9a7cfb3f81602f36288e76df321e4fb
Maksym Dilai <admin@dproject.org.ua>
2017-05-11 08:20:20 +0000

    Port fix

--- p2pool/networks/bitcoin.py
+++ p2pool/networks/bitcoin.py
@@ -14,11 +14,11 @@ TARGET_LOOKBEHIND = 200 # shares
 SPREAD = 3 # blocks
 IDENTIFIER = 'fc70035c7a81bc6f'.decode('hex')
 PREFIX = '2472ef181efcd37b'.decode('hex')
-P2P_PORT = 9333
+P2P_PORT = 9335
 MIN_TARGET = 0
 MAX_TARGET = 2**256//2**32 - 1
 PERSIST = True
-WORKER_PORT = 9332
+WORKER_PORT = 9334
 BOOTSTRAP_ADDRS = 'ml.toom.im ml.toom.im:9336 ml.toom.im:9334 93.77.99.166 whortonda.noip.me:9333 btc-fork.coinpool.pw:9335 crypto.office-on-the.net:9335'.split(' ')
 ANNOUNCE_CHANNEL = '#p2pool'
 VERSION_CHECK = lambda v: None if 100000 <= v else 'Bitcoin version too old. Upgrade to 0.11.2 or newer!' # not a bug. BIP65 support is ensured by SOFTFORKS_REQUIRED
