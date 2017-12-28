910b46e6ab55211c2732c9d2270cae5d69ee5b50
Paul Georgiou <pavlos1998@gmail.com>
2017-12-28 14:14:10 +0200

    Don't mine txs with unconfirmed parents
    Reduces risk of creating invalid blocks

--- p2pool/bitcoin/helper.py
+++ p2pool/bitcoin/helper.py
@@ -112,6 +112,7 @@ def getwork(bitcoind, use_getblocktemplate=False, txidcache={}, feecache={}, fee
         fum = 100000
         while len(feefifo) > fum:
             del feecache[feefifo.pop(0)]
+    packed_transactions = [x['data'].decode('hex') for x in work['transactions'] if len(x.get('depends', [])) == 0]
     if 'height' not in work:
         work['height'] = (yield bitcoind.rpc_getblock(work['previousblockhash']))['height'] + 1
     elif p2pool.DEBUG:
