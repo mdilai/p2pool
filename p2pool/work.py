b190c563921b20758d65b3f30e9ab9e279e57bd3
Jonathan Toomim <j@toom.im>
2018-01-21 17:42:44 -0800

    Decrease default pseudoshare target from 1/sec to 10/sec

--- p2pool/work.py
+++ p2pool/work.py
@@ -347,14 +347,14 @@ class WorkerBridge(worker_interface.WorkerBridge):
             local_hash_rate = self._estimate_local_hash_rate()
             if local_hash_rate is not None:
                 target = min(target,
-                    bitcoin_data.average_attempts_to_target(local_hash_rate * 1)) # limit to 1 share response every second by modulating pseudoshare difficulty
+                    bitcoin_data.average_attempts_to_target(local_hash_rate * .1)) # target 10 share responses every second by modulating pseudoshare difficulty
             else:
                 # If we don't yet have an estimated node hashrate, then we still need to not undershoot the difficulty.
                 # Otherwise, we might get 1 PH/s of hashrate on difficulty settings appropriate for 1 GH/s.
-                # 1/100th the difficulty of a full share should be a reasonable upper bound. That way, if
+                # 1/3000th the difficulty of a full share should be a reasonable upper bound. That way, if
                 # one node has the whole p2pool hashrate, it will still only need to process one pseudoshare
-                # every 0.3 seconds.
-                target = min(target, 1000 * bitcoin_data.average_attempts_to_target((bitcoin_data.target_to_average_attempts(
+                # every ~0.01 seconds.
+                target = min(target, 3000 * bitcoin_data.average_attempts_to_target((bitcoin_data.target_to_average_attempts(
                     self.node.bitcoind_work.value['bits'].target)*self.node.net.SPREAD)*self.node.net.PARENT.DUST_THRESHOLD/block_subsidy))
         else:
             target = desired_pseudoshare_target
